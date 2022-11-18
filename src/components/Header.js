8f83f23cdad96451d4f046d3c747c893d01592b7
Kaamil Ahamadh <kaamilahamadh6@gmail.com>
2022-11-18 22:56:38 +0530

    Fix Title Click for HomeScreen

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { MdOutlineSlowMotionVideo } from "react-icons/md";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const navigate = useNavigate();
   return (
-    <div className="header-container">
+    <div className="header-container" onClick={() => navigate("/")}>
       <MdOutlineSlowMotionVideo size={25} className="header-icon" />
       <div>Play Videos Hub </div>
     </div>
